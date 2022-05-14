a513109fd914d02d8f76c44104c8af9d27988ed6
nirzaf <mfmfazrin1986@gmail.com>
2022-05-14 15:58:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+
+//WriteLine("Hello, World!");
+
+//string myName;
+
+//int number = 34;
+
+//int maxNum = int.MaxValue;
+
+//WriteLine(maxNum);
+
+//float maxFloatValue = float.MaxValue;
+
+//WriteLine(maxFloatValue);
+
+//double Number = 5.323244343;
+
+//long maxLongValue = long.MaxValue;
+
+//const string name = "John";
+
+//char myChar = 'a';
+
+//bool myBool = true;
+
+//long allocates 64 bits in memory
+//ULong allocates 128 bits in memory
+
+//WriteLine("Enter Number 1");
+
+//int num1 = Convert.ToInt32(ReadLine());
+
+//WriteLine("Enter Number 2");
+
+//int num2 = Convert.ToInt32(ReadLine());
+
+//WriteLine(num1 + num2);
+//WriteLine(num1 - num2);
+//WriteLine(num1 * num2);
+//WriteLine(num1 / num2);
+
+string firstName = ReadLine();
+string lastName = ReadLine();
+
+WriteLine("Hello, " + firstName + " " + lastName);
+
+
