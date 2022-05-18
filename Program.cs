b0594cf3e52c269bd874e76d326d3399335d8ba0
nirzaf <mfmfazrin1986@gmail.com>
2022-05-18 20:23:53 +0530

    //Create a for loop to print numbers from 1 to 10 WriteLine("Printing numbers from 1 to 10 from for loop"); for (int i = 1; i <= 10; i++) {     WriteLine(i); }

--- Program.cs
+++ Program.cs
@@ -25,6 +25,32 @@ for (int i = 1; i <= 10; i++)
     WriteLine(i);
 }
 
+//Create a foreach loop to print numbers from 1 to 10
+WriteLine("Printing numbers from 1 to 10 from foreach loop");
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (int i in numbers)
+{
+    WriteLine(i);
+}
+
+//Create a recursive function to print numbers from 1 to 10
+WriteLine("Printing numbers from 1 to 10 from recursive function");
+
+Numbers.Print(5);
+
+public static class Numbers
+{
+    public static void Print(int number)
+    {
+        if (number <= 10)
+        {
+            WriteLine(number);
+            Print(number + 1);
+        }
+    }
+}
 
 
 
