c5752b338aaa7bf1439c29a90dd358d5bd3cf74a
nirzaf <mfmfazrin1986@gmail.com>
2022-05-18 20:09:48 +0530

    //Create a for loop to print numbers from 1 to 10 WriteLine("Printing numbers from 1 to 10 from for loop"); for (int i = 1; i <= 10; i++) {     WriteLine(i); }

--- Program.cs
+++ Program.cs
@@ -1,48 +1,29 @@
 ﻿using static System.Console;
 
-WriteLine("Hello, World!");
+//Create a while loop to print numbers from 1 to 10
+int number = 1;
+WriteLine("Printing numbers from 1 to 10 from while loop");
+while (number <= 10)
+{
+    WriteLine(number);
+    number++;
+}
 
-string myName;
+//Create a Do while loop to print numbers from 1 to 10
+int count = 1;
+WriteLine("Printing numbers from 1 to 10 from do while loop");
+do
+{
+    WriteLine(count);
+    count++;
+} while (count <= 10);
 
-int number = 34;
-
-int maxNum = int.MaxValue;
-
-WriteLine(maxNum);
-
-float maxFloatValue = float.MaxValue;
-
-WriteLine(maxFloatValue);
-
-double Number = 5.323244343;
-
-long maxLongValue = long.MaxValue;
-
-const string name = "John";
-
-WriteLine(name);
-
-char myChar = 'a';
-
-bool myBool = true;
-
-// A long value is stored in 64-bit,with its first digit to show if it's a positive/negative number.
-// while ulong is also 64-bit, with all 64 bit to store the number.
-// so the maximum of ulong is 2(64)-1, while long is 2(63)-1
-
-WriteLine("Enter Number 1");
-
-int num1 = Convert.ToInt32(ReadLine());
-
-WriteLine("Enter Number 2");
-
-int num2 = Convert.ToInt32(ReadLine());
-
-WriteLine(num1 + num2);
-WriteLine(num1 - num2);
-WriteLine(num1 * num2);
-WriteLine(num1 / num2);
-
+//Create a for loop to print numbers from 1 to 10
+WriteLine("Printing numbers from 1 to 10 from for loop");
+for (int i = 1; i <= 10; i++)
+{
+    WriteLine(i);
+}
 
 
 
