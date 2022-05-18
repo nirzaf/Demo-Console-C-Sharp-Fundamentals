e5095aa943000babeabed8bc8dea9995a5317a99
nirzaf <mfmfazrin1986@gmail.com>
2022-05-18 18:21:12 +0530

    commit switch statements

--- Program.cs
+++ Program.cs
@@ -1,114 +1,37 @@
 ﻿using static System.Console;
 
-//WriteLine("Hello, World!");
 
-//string myName;
+WriteLine("Enter a number : ");
 
-//int number = 34;
+int x = Convert.ToInt32(ReadLine());
 
-//int maxNum = int.MaxValue;
+WriteLine("Enter a symbol : ");
 
-//WriteLine(maxNum);
+char symbol = Convert.ToChar(ReadLine());
 
-//float maxFloatValue = float.MaxValue;
+WriteLine("Enter a number 2 : ");
 
-//WriteLine(maxFloatValue);
+int y = Convert.ToInt32(ReadLine());
 
-//double Number = 5.323244343;
-
-//long maxLongValue = long.MaxValue;
-
-//const string name = "John";
-
-//char myChar = 'a';
-
-//bool myBool = true;
-
-//long allocates 64 bits in memory
-//ULong allocates 128 bits in memory
-
-//WriteLine("Enter Number 1");
-
-//int num1 = Convert.ToInt32(ReadLine());
-
-//WriteLine("Enter Number 2");
-
-//int num2 = Convert.ToInt32(ReadLine());
-
-//WriteLine(num1 + num2);
-//WriteLine(num1 - num2);
-//WriteLine(num1 * num2);
-//WriteLine(num1 / num2);
-
-//string firstName = ReadLine();
-//string lastName = ReadLine();
-
-//WriteLine("Hello, " + firstName + " " + lastName);
-
-//WriteLine("Enter a number : ");
-
-//int x = Convert.ToInt32(ReadLine());
-
-//WriteLine("Enter a symbol : ");
-
-//char symbol = Convert.ToChar(ReadLine());
-
-//WriteLine("Enter a number 2 : ");
-
-//int y = Convert.ToInt32(ReadLine());
-
-//if (symbol == '+')
-//{
-//    WriteLine(x+y);
-//}
-//else if (symbol == '-')
-//{
-//    WriteLine(x - y);
-//}
-//else if (symbol == '*')
-//{
-//    WriteLine(x * y);
-//}
-//else if (symbol == '/')
-//{
-//    WriteLine(x / y);
-//}
-//else 
-//{
-//    WriteLine("Invalid Symbol");
-//}
-
-//int currentBalance = 1000;
-
-//WriteLine("Enter amount to withdraw : ");
-
-//int amount = Convert.ToInt32(ReadLine());
-
-//if (amount <= currentBalance)
-//{
-//    WriteLine("Withdrawal Successful");
-//    currentBalance = currentBalance - amount;
-//    WriteLine("Current Balance : " + currentBalance);
-//}
-//else
-//{
-//    WriteLine("Insufficient Funds");
-//}
-
-WriteLine("Enter your age : ");
-int age = Convert.ToInt32(ReadLine());
-
-WriteLine("Enter your marks");
-int marks = Convert.ToInt32(ReadLine());;
-
-if (age > 18 || marks > 50)
+switch (symbol)
 {
-    WriteLine("You are eligible for Scholarship");
+    case '+':
+        WriteLine(x + y);
+        break;
+    case '-':
+        WriteLine(x - y);
+        break;
+    case '*':
+        WriteLine(x * y);
+        break;
+    case '/':
+        WriteLine(x / y);
+        break;
+    default:
+        WriteLine("Invalid Symbol");
+        break;
 }
-else
-{
-    WriteLine("You are not eligible for Scholarship");
-}
+
 
 
 
