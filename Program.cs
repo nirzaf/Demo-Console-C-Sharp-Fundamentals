d58152f9341fd8ad8b71285ebd86068a04135d62
nirzaf <mfmfazrin1986@gmail.com>
2022-05-21 16:11:44 +0530

    //Create a foreach loop to print numbers from 1 to 10

--- Program.cs
+++ Program.cs
@@ -1,32 +1,41 @@
 ﻿using static System.Console;
 
+
 //Create a while loop to print numbers from 1 to 10
-int number = 1;
-WriteLine("Printing numbers from 1 to 10 from while loop");
-while (number <= 10)
-{
-    WriteLine(number);
-    number++;
-}
+
+//int number = 11;
+//WriteLine("Printing numbers from 1 to 10 from while loop");
+//while (number <= 10)
+//{
+//    WriteLine($"You have entered number : {number}");
+//    number++;
+//}
 
 //Create a Do while loop to print numbers from 1 to 10
-int count = 1;
-WriteLine("Printing numbers from 1 to 10 from do while loop");
-do
-{
-    WriteLine(count);
-    count++;
-} while (count <= 10);
+//int count = 11;
+//WriteLine("Printing numbers from 1 to 10 from do while loop");
+//do
+//{
+//    WriteLine(count);
+//    count++;
+//    //count = count + 2;
+//    //count += 2;
+//    //count = count - 2 == count -= 2;
+//} while (count <= 10);
+
+
 
 //Create a for loop to print numbers from 1 to 10
-WriteLine("Printing numbers from 1 to 10 from for loop");
-for (int i = 1; i <= 10; i++)
-{
-    WriteLine(i);
-}
+//WriteLine("Printing numbers from 1 to 10 from for loop");
+//for (int i = 1; i <= 10; i+=3)
+//{
+//    WriteLine(i);
+//}
 
 //Create a foreach loop to print numbers from 1 to 10
 WriteLine("Printing numbers from 1 to 10 from foreach loop");
+
+//string[] names = { "John", "Jane", "Joe" };
 
 int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
