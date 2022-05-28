baabca5d42f11ba972bce38adff19302f8a6299c
Fazrin <mfmfazrin1986@gmail.com>
2022-05-28 17:23:18 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,74 +1,22 @@
-﻿using static System.Console;
+﻿
+using Demo_Console_C_Sharp_Fundamentals;
+using static System.Console;
 
-//List<string> names = new() { "John", "Paul", "George", "Ringo" };
+//Initiating a new instance of the vehicle class
+Vehicle car = new ();
 
-//foreach (var name in names)
-//{
-//    WriteLine(name);
-//}
+//Object name is car
+car.VehicleNumber = 5102;
+car.VehicleMaxSpeed = 100;
+car.VehicleColor = "Black";
+car.VehicleType = "Auto";
+car.IsVehicleRegistered = true;
 
-//WriteLine("------------------");
-//names.Add("Fazrin");
-//names.ForEach(WriteLine);
-
-//List<string> addedNames = new() { "Mohamed", "Abdul", "Ahmed" };
-//names.AddRange(addedNames);
-
-//names.ForEach(WriteLine);
-
-List<Student> students = new()
-{
-    new() { Name = "John", Age = 18, City = "London", Grade = 10 },
-    new() { Name = "Paul", Age = 20 , City = "Paris", Grade = 12 },
-    new() { Name = "George", Age = 21, City = "Berlin", Grade = 11 },
-    new() { Name = "Ringo", Age = 25, City = "Liverpool", Grade = 9 },
-    new() { Name = "Fazrin", Age = 22, City = "Manchester", Grade = 8 }
-};
-
-//var filteredStudents = students.Where(c => c.Age >= 21);
-
-//foreach (var stu in filteredStudents)
-//{
-//    WriteLine(" Name: " + stu.Name + " Age: " + stu.Age + " City: " + stu.City + " Grade: " + stu.Grade);
-//}
-
-//foreach (var stu in students)
-//{
-//    WriteLine(" Name: " + stu.Name +" Age: " +stu.Age + " City: " + stu.City + " Grade: "+ stu.Grade );
-//}
+//WriteLine(car.VehicleNumber);
 
 
-
-Tuple<int, string, string> person1 = new(1, "Steve", "Jobs");
-Tuple<int, string, string> person2 = new(2, "Bill", "Gates");
-Tuple<int, string, string> person3 = new(3, "Satya", "Nadella");
-Tuple<int, string, string> person4 = new(4, "Elon", "Musk");
-Tuple<int, string, string> person5 = new(5, "Mohamed", "Fazrin");
-
-WriteLine(person1.Item1);
-WriteLine(person1.Item2);
-WriteLine(person4.Item3);
-
-ReadLine();
-record Student
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string City { get; set; }
-    public int Grade { get; set; }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+car.PrintVehicleDetails();
+car.ChangeVehicleType("Manual");
+WriteLine("-----------------------------------");
+car.PrintVehicleDetails();
 
---
+++ Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace Demo_Console_C_Sharp_Fundamentals
+{
+    public class Vehicle
+    {
+        public int VehicleNumber { get; set; }
+        public string VehicleType { get; set; }
+        public string VehicleColor { get; set; }
+        public double VehicleMaxSpeed { get; set; }
+        public bool IsVehicleRegistered { get; set; }
+
+        public void PrintVehicleDetails()
+        {
+            WriteLine("Vehicle Number: {0}", VehicleNumber);
+            WriteLine("Vehicle Type: {0}", VehicleType);
+            WriteLine("Vehicle Color: {0}", VehicleColor);
+            WriteLine("Vehicle Max Speed: {0}", VehicleMaxSpeed);
+            WriteLine("Is Vehicle Registered: {0}", IsVehicleRegistered);
+        }
+
+        public void ChangeVehicleType(string type)
+        {
+            VehicleType = type;
+        }
+
+    }
+}
