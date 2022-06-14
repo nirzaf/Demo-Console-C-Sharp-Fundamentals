a7d8d81070bd9b220ad38bad5e2ffa7895155938
nirzaf <mfmfazrin1986@gmail.com>
2022-06-14 19:56:49 +0530

    static void MultiplicationTable(int n) {     for(int i=1; i<=n; i++)     {         int ans = 1 * i;         Console.WriteLine("{0} X {1} = {2}", i, 1, ans);     } }

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
 ﻿using Demo_Console_C_Sharp_Fundamentals;
 
+
+var input = Console.ReadLine();
+
+MultiplicationTable(Convert.ToInt32(input));
+
+/*
+ Write a program in C# to display the multiplication table
+ vertically from 1 to n
+ */
+
+static void MultiplicationTable(int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        int ans = 1 * i;
+        Console.WriteLine("{0} X {1} = {2}", i, 1, ans);
+    }
+}
+
+
 Vehicle v1 = new()
 {
     VehicleNumber = 12121,
