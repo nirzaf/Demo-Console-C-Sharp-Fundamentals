182dc50978e849cf3f8954fb235d7e397a08b396
nirzaf <mfmfazrin1986@gmail.com>
2022-06-16 08:46:03 +0530

    pushed by mfmfazrin1986@gmail.com at Thursday, June 16, 2022, 08:46:02 AM

--- .history/Program_20220616084559.cs
+++ .history/Program_20220616084559.cs
@@ -1,6 +1,4 @@
-﻿
-
-// var input = Console.ReadLine();
+﻿// var input = Console.ReadLine();
 // MultiplicationTable(Convert.ToInt32(input));
 
 // int[] numbers =  { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
