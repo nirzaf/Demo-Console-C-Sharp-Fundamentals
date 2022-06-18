650d87d67f556161b99983cc96b31bad5681872c
nirzaf <mfmfazrin1986@gmail.com>
2022-06-18 17:39:01 +0530

    pushed by mfmfazrin1986@gmail.com at Saturday, June 18, 2022, 05:39:01 PM

---
+++ .history/Employee_20220618173731.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Console_C_Sharp_Fundamentals
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BasicSalary { get;set; }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public double 
+    }
+}
