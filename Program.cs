4b37b890f4050d95b6ee8bc31b02e0b3dae87194
nirzaf <mfmfazrin1986@gmail.com>
2022-06-21 08:45:53 +0530

    //Selecting a list of distinct category names

--- Program.cs
+++ Program.cs
@@ -4,8 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Samsung s1 = new ();
-        s1.Print();
+        var products = new List<Product>
+        {
+            new("Strawberry", new List<string> {"Fruit"}),
+            new("Banana", new List<string> {"Fruit", "Yellow"}),
+            new("Apple", new List<string> {"Fruit", "Red"}),
+            new("Orange", new List<string> {"Fruit", "Orange"}),
+            new("Grape", new List<string> {"Fruit", "Red"}),
+        };
+        
+        //Selecting a list of distinct category names
+        var categoryNames = products.SelectMany(p => p.Categories).Distinct().ToList();
+
+        foreach (var item in categoryNames)
+        {
+            Console.WriteLine(item);
+        }
+
         Console.ReadLine();
     }
 }
+
+public record Product(string Name, List<string> Categories);
+
